321d7dfb43af3833d076e1cc05b1378204e7fef6
uptonking <jinyaoo86@gmail.com>
2025-11-01 03:07:24 +0800

    refactor: aisdk eg routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
+      <main className='row-start-2 flex flex-col items-center gap-[32px] sm:items-start'>
         {/* <Image
           className="dark:invert"
           src="/next.svg"
@@ -13,23 +13,22 @@ export default function Home() {
           height={38}
           priority
         /> */}
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          {/* <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li> */}
-          <li className="tracking-[-.01em]">
+        <ol className='list-inside list-decimal text-center font-mono text-sm/6 sm:text-left'>
+          <li className='tracking-[-.01em]'>
             <Link
-              href="/chat"
-              className="text-blue-600 dark:text-blue-400 hover:underline"
+              href='/starter'
+              className='text-blue-600 hover:underline dark:text-blue-400'
             >
-              Try to chat with AI(OpenAI-compatible, like Groq...)
+              Quick start webapp to chat with AI(OpenAI-compatible, like
+              Groq...)
+            </Link>
+          </li>
+          <li className='tracking-[-.01em]'>
+            <Link
+              href='/chat'
+              className='text-blue-600 hover:underline dark:text-blue-400'
+            >
+              Chat persistence with local file
             </Link>
           </li>
         </ol>
